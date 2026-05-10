3130b8540001e53d1dd200342cccedc93887161d
Lenny Gitonga <lennygitonga07@gmail.com>
2026-05-11 02:37:18 +0300

    Update SECRET_KEY setting to use environment variable for enhanced security

--- calorie_project/settings.py
+++ calorie_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#7vt+=2zne9m@wfqfmx#)yzkb$+@*a&ljhi9lkl12(a^i3%fzm'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-for-local')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
