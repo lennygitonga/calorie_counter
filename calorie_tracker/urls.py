313e13ea1469ac52d3d796e3a985576bd5ffe352
Lenny Gitonga <lennygitonga07@gmail.com>
2026-05-11 01:09:18 +0300

    Add URL configuration for calorie_tracker app

---
+++ calorie_tracker/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add/', views.add_food, name='add_food'),
+    path('delete/<int:food_id>/', views.delete_food, name='delete_food'),
+    path('reset/', views.reset_calories, name='reset_calories'),
+]
