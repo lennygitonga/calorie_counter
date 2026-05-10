19be0c8b05eb64a279e81ae57cc671f740c6e7bf
Lenny Gitonga <lennygitonga07@gmail.com>
2026-05-11 01:35:18 +0300

    Add lookup-calories route to urlpatterns for calorie estimation

--- calorie_tracker/urls.py
+++ calorie_tracker/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add/', views.add_food, name='add_food'),
     path('delete/<int:food_id>/', views.delete_food, name='delete_food'),
     path('reset/', views.reset_calories, name='reset_calories'),
+    path('lookup-calories/', views.lookup_calories, name='lookup_calories'),
 ]
