b743155077154499f144119b777538d6a6c36f9e
Lenny Gitonga <lennygitonga07@gmail.com>
2026-05-11 02:08:37 +0300

    Update settings for allowed hosts and static files; add build script for deployment

--- calorie_project/settings.py
+++ calorie_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-#7vt+=2zne9m@wfqfmx#)yzkb$+@*a&ljhi9lkl12(a^i3%fzm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,4 +123,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
