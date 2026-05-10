9d30806c23129b597083591b249ad1be84a1dd15
Lenny Gitonga <lennygitonga07@gmail.com>
2026-05-11 01:06:18 +0300

    Add FoodItem model and initial migration

--- calorie_tracker/models.py
+++ calorie_tracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class FoodItem(models.Model):
+    """Represents a single food item and its calorie count."""
+    
+    name = models.CharField(max_length=200)
+    calories = models.PositiveIntegerField()
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.calories} calories"
